9b1a4ff72cd8375cb104c88923248056ae161342
Tugberk Ugurlu <tugberk@outlook.com>
2016-05-31 07:56:43 -0700

    init cli app

--- zippy/main.go
+++ zippy/main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Print("hello and byee!\n")
-	os.Exit(-1)
+
+	app := cli.NewApp()
+	app.Name = "zippy"
+	app.Author = "Tugberk Ugurlu"
+	app.Usage = "General purpose package distribution tool for your stuff (yes, all your stuff™)"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Print("hello and byee!\n")
+		return cli.NewExitError("no pun intended", -1)
+	}
+
+	app.Run(os.Args)
 }
