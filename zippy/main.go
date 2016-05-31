4bf595aa32f97e3767cd683c66f2dba3722c1bc4
Tugberk Ugurlu <tugberk@outlook.com>
2016-05-31 09:59:57 -0700

    install command

--- zippy/main.go
+++ zippy/main.go
@@ -14,6 +14,30 @@ func main() {
 	app.Author = "Tugberk Ugurlu"
 	app.Usage = "General purpose package distribution tool for your stuff (yes, all your stuff™)"
 
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:  "install",
+			Usage: "Downloads the specified package and outputs it onto the specified directory",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "version, v",
+					Usage: "Version of the package to be installed",
+				},
+
+				cli.StringFlag{
+					Name:  "outputFolder, o",
+					Usage: "Path to the output folder for the package to be installed",
+					Value: "./",
+				},
+
+				cli.StringFlag{
+					Name:  "source, s",
+					Usage: "Base URL for the desired zippy endpoint to be looked at for this install operation",
+				},
+			},
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		fmt.Print("hello and byee!\n")
 		return cli.NewExitError("no pun intended", -1)
