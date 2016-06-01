7151ac6d10a4c23ccc2b8897ecdf67883fadcab2
Tugberk Ugurlu <tugberk@outlook.com>
2016-06-01 03:56:06 -0700

    initial server

--- zippy-server/main.go
+++ zippy-server/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Print("hello and byee!\n")
-	os.Exit(-1)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
