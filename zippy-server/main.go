dbe5440d68d1de01a194b590c3ce1ca4103bff5d
Tugberk Ugurlu <tugberk@outlook.com>
2016-06-01 04:02:14 -0700

    router

--- zippy-server/main.go
+++ zippy-server/main.go
@@ -5,13 +5,22 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
